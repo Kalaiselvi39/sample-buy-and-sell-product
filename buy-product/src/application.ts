708ca669a68853cb6c8beea7c4c32cd3874a97b6
Kalaiselvi S <kalaisakthi169@gmail.com>
2025-10-09 23:26:24 +0530

    updtae with bind the api url

--- buy-product/src/application.ts
+++ buy-product/src/application.ts
@@ -1,5 +1,5 @@
 import {BootMixin} from '@loopback/boot';
-import {ApplicationConfig} from '@loopback/core';
+import {ApplicationConfig,BindingKey} from '@loopback/core';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
@@ -11,15 +11,24 @@ import path from 'path';
 import {MySequence} from './sequence';
 import {PurchaseService} from './services/purchase.service';
 import {BuyRepository} from './repositories/buy.repository';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export {ApplicationConfig};
 
+export const CONFIG = {
+  SELLER_API_URL: BindingKey.create<string>('config.sellerApiUrl'),
+};
 export class BuyProductApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
 
+    // Bind environment-based config values
+    this.bind('config.sellerApiUrl').to(
+  process.env.SELLER_API_URL || 'http://127.0.0.1:4001',
+);
     // Set up the custom sequence
     this.sequence(MySequence);
 
@@ -44,5 +53,10 @@ export class BuyProductApplication extends BootMixin(
     // Register repository and services for Buy module
     this.repository(BuyRepository);
     this.bind('services.PurchaseService').toClass(PurchaseService);
+
+      console.log(
+  `Billing Service started with SELLER_API_URL: ${this.getSync(CONFIG.SELLER_API_URL)}`,
+);
   }
+  
 }
